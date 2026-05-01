116693684bf48dc479389096eb3f515dc8c83e3e
1PoPTRoN <vrxn.arp1traj@gmail.com>
2026-05-01 10:00:31 +0530

    Gate 1: Gradle skeleton with JUnit 5 smoke test
    Signed-off-by: 1PoPTRoN <vrxn.arp1traj@gmail.com>

---
+++ src/test/java/io/lfdt/paladin/poc/SmokeTest.java
@@ -0,0 +1,13 @@
+package io.lfdt.paladin.poc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+
+    @Test
+    void buildIsAlive() {
+        assertEquals(2, 1 + 1);
+    }
+}
